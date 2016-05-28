cb50efb746ba7522ade2fb6df8946902d536f1e3
Spider2themax <maxwell.p.henderson@gmail.com>
2016-05-28 18:02:07 -0400

    Missed a file

--- GetDisplacements.m
+++ GetDisplacements.m
@@ -1,13 +1,12 @@
-function [r_net] = GetDisplacements(C, pos1, pos2)
+function [r_net] = GetDisplacements(pos1, pos2)
 
 %% Calculate RMS displacements for all neurons in network.
 L = 512; % length of box hard coded cause i am lazy for now
 r_net = zeros(length(pos1),1);
 count = 1;
-for i = 1:length(C),
-    for j = 1:length(C),
-            r_net(count) = CalcPeriodicDist(pos1(i,:),pos2(i,:),L) + CalcPeriodicDist(pos1(j,:),pos2(j,:),L);
-            count = count + 1;
-        end
+for i = 1:length(pos1),
+    for j = 1:length(pos1),
+        r_net(count) = CalcPeriodicDist(pos1(i,:),pos2(i,:),L) + CalcPeriodicDist(pos1(j,:),pos2(j,:),L);
+        count = count + 1;
     end
 end
